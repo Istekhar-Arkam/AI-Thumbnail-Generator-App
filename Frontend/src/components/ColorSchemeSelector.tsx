3df1ba985a6dd03d949f20620928b22d61a9ca06
istekhar <istekhararkam25@gmail.com>
2026-01-17 18:49:13 +0530

    feat: Refactor ColorSchemeSelector to accept value and onChange props for better integration

--- Frontend/src/components/ColorSchemeSelector.tsx
+++ Frontend/src/components/ColorSchemeSelector.tsx
@@ -1,9 +1,28 @@
+import { colorSchemes } from "../assets/assets";
 
-
-const ColorSchemeSelector = () => {
+const ColorSchemeSelector = ({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (color: string) => void;
+}) => {
   return (
-    <div>ColorSchemeSelector</div>
-  )
-}
+    <div className="space-y-3">
+      <label className="block text-sm font-medium text-zinc-200">
+        Color Scheme
+      </label>
+      <div className="grid grid-cols-6 gap-3">
+        {colorSchemes.map((scheme) => (
+          <button
+            key={scheme.id}
+            onClick={() => onChange(scheme.id)}
+            className={`relative rounded-lg transition-all ${value === scheme.id && "ring-2 ring-pink-500"}`}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default ColorSchemeSelector
+export default ColorSchemeSelector;
