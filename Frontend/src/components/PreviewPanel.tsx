d2bd16ee02275bc125f124aaca0a867c724022c7
istekhar <istekhararkam25@gmail.com>
2026-01-19 22:05:13 +0530

    feat: Refactor PreviewPanel and Generate components for improved thumbnail handling and loading state

--- Frontend/src/components/PreviewPanel.tsx
+++ Frontend/src/components/PreviewPanel.tsx
@@ -1,11 +1,11 @@
 import type { IThumbnail, AspectRatio } from "../assets/assets";
-
+import { Loader2Icon, DownloadIcon, ImageIcon } from "lucide-react";
 const PreviewPanel = ({
   thumbnail,
   isLoading,
   aspectRatio,
 }: {
-  thumbnail: IThumbnail;
+  thumbnail: IThumbnail | null;
   isLoading: boolean;
   aspectRatio: AspectRatio;
 }) => {
@@ -15,10 +15,10 @@ const PreviewPanel = ({
     "9:16": "aspect=[9/16]",
   } as Record<AspectRatio, string>;
 
-
-const onDownload=()=>{
-  if(!thumbnail?.image_url)
-}
+  const onDownload = () => {
+    if (!thumbnail?.image_url) return;
+    window.open(thumbnail.image_url, "_blank");
+  };
 
   return (
     <div className="relative mx-auto w-full max-w-2xl">
@@ -48,13 +48,31 @@ const onDownload=()=>{
             />
             <div className="absolute inset-0 flex items-end justify-center bg-black/10 opacity-0 transition-opacity group-hover:opacity-100">
               <button
-              onClick={onDownload}
+                onClick={onDownload}
                 type="button"
                 className="mb-6 flex items-center gap-2 rounded-md px-5 py-2.5 text-xs font-medium transition bg-white/30 ring-2 ring-white/40 backdrop-blur hover:scale-105 active:scale-95"
               >
                 <DownloadIcon className="size-4" />
                 Download Thumbnail
               </button>
+            </div>
+          </div>
+        )}
+
+        {/* empty state */}
+
+        {!isLoading && !thumbnail?.image_url && (
+          <div className="absolute inset-0 m-2 flex flex-col items-center justify-center gap-4 rounded-lg border-2 border-dashed border-white/20 bg-black/25">
+            <div className="max-sm:hidden flex size-20 items-center justify-center rounded-full bg-white/10">
+              <ImageIcon className="size-10 text-white opacity-50" />
+            </div>
+            <div className="px-4 text-center">
+              <p className="font text-zinc-200">
+                Generate your first thumbnail
+              </p>
+              <p className="mt-1 text-xs text-zinc-400">
+                Fill out the form and click Generate
+              </p>
             </div>
           </div>
         )}
