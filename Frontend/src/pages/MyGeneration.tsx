d3a7ebab30d30ff5f2cbc3f1c0f54df0a3481622
istekhar <istekhararkam25@gmail.com>
2026-01-20 12:14:29 +0530

    feat: Update MyGeneration page to include thumbnail fetching and management features

--- Frontend/src/pages/MyGeneration.tsx
+++ Frontend/src/pages/MyGeneration.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react"
+import SoftBackdrop from "../components/SoftBackdrop"
+import  {type IThumbnail ,dummyThumbnails} from "../assets/assets"
 
 const MyGeneration = () => {
+
+  const [thumbnails,setThumbnails]=useState<IThumbnail>([])
+  const [loading,setLoading]=useState(false)
+
+  const fetchThumbnails=async()=>{
+setThumbnails(dummyThumbnails as unknown as IThumbnail[])
+setLoading(false)
+  }
+
+const handleDownload=(image_url:string)=>{
+  window.open(image_url,"_blank")
+}
+
+const handleDelete=async(id:string)=>{
+  console.log(id)
+}
+
+  useEffect(()=>{
+fetchThumbnails()
+  },[])
   return (
-    <div>MyGeneration</div>
+    <>
+<SoftBackdrop/>
+<div className="mt-32 min-h-screen px-6 md:px-16 lg:px-24 xl:px-32">
+{/* header */}
+<div className="mb-8">
+  <h1 className="text-2xl font-bold text-zinc-200">
+    My Generation
+  </h1>
+  <p className="text-sm text-zinc-400 mt-1">View and manage all your AI-generated thumbnails</p>
+
+</div>
+</div>
+    </>
   )
 }
 
