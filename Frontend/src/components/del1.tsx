8d9054e875c9842fbcf94c7ed33d474af8014c6d
istekhar <istekhararkam25@gmail.com>
2026-01-25 21:41:41 +0530

    feat: add del1 and del2 components with basic structure

---
+++ Frontend/src/components/del1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const del1 = () => {
+  return (
+    <div>del1</div>
+  )
+}
+
+export default del1
---
+++ Frontend/src/components/del2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const del2 = () => {
+  return (
+    <div>del2</div>
+  )
+}
+
+export default del2
