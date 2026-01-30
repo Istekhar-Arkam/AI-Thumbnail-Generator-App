46459e26bfac1422fa400180abf0eb3549fc06a3
istekhar <istekhararkam25@gmail.com>
2026-01-30 20:23:02 +0530

    feat: remove del1, del2, and del3 components

--- Frontend/src/components/del2.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const del2 = () => {
-  return (
-    <div>del2</div>
-  )
-}
-
-export default del2
--- Frontend/src/components/del3.tsx
+++
@@ -1,9 +0,0 @@
-
-
-const del3 = () => {
-  return (
-    <div>del3</div>
-  )
-}
-
-export default del3
