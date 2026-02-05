20e1e1b67b831be7045e8b99cd1233ac8e18a1f0
istekhar <istekhararkam25@gmail.com>
2026-02-05 19:34:44 +0530

    Add @google/genai dependency to Backend package.json

--- Backend/controllers/ThumbnailController.ts
+++ Backend/controllers/ThumbnailController.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 import Thumbnail from "../models/Thumbnail.js";
+import {
+  GenerateContentConfig,
+  HarmBlockThreshold,
+  HarmCategory,
+} from "@google/genai";
 
 export const generateThumbnail = async (req: Request, res: Response) => {
   try {
@@ -23,5 +28,35 @@ export const generateThumbnail = async (req: Request, res: Response) => {
       text_overlay,
       isGenerating: true,
     });
+
+    const model = "gemini-3-pro-image-preview";
+    const generationConfig: GenerateContentConfig = {
+      maxOutputTokens: 32768,
+      temperature: 1,
+      topP: 0.95,
+      responseModalities: ["image"],
+      imageConfig: {
+        aspectRatio: "16.9",
+        imageSize: "1K",
+      },
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.OFF,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.OFF,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.OFF,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.OFF,
+        },
+      ],
+    };
   } catch (error) {}
 };
